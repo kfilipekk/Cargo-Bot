90c5a88e474453d69b6b0141852f858f00a4f300
Krystian Filipek <kf446@cam.ac.uk>
2025-10-17 20:06:26 +0100

    Added more robust motor code and added simultaneous motor movement

--- sw/motor.py
+++ sw/motor.py
@@ -1,35 +1,56 @@
-from machine import Pin, PWM
+from machine import Pin , PWM
 from utime import sleep
 
-class Motor:
-    def __init__(self, dirPin, PWMPin):
-        self.mDir = Pin(dirPin, Pin.OUT)  # set motor direction pin
-        self.pwm = PWM(Pin(PWMPin))  # set motor pwm pin
-        self.pwm.freq(1000)  # set PWM frequency
-        self.pwm.duty_u16(0)  # set duty cycle - 0=off
-        
-    def off(self):
-        self.pwm.duty_u16(0)
-    def Forward(self, speed=100):
-        self.mDir.value(0)                     # forward = 0 reverse = 1 motor
-        self.pwm.duty_u16(int(65535 * speed / 100))  # speed range 0-100 motor
+# Motor A
+led = Pin(25,Pin.OUT)
+ina1 = Pin(5,Pin.OUT)
+ina2 = Pin(4, Pin.OUT)
+pwma = PWM(Pin(5))
+pwma.freq(1000)
 
-    def Reverse(self, speed=30):
-        self.mDir.value(1)
-        self.pwm.duty_u16(int(65535 * speed / 100))
+# Motor B
+inb1 = Pin(6, Pin.OUT)
+inb2 = Pin(7, Pin.OUT)
+pwmb = PWM(Pin(6))
+pwmb.freq(1000)
+
+led.toggle()
 
 
-def test_motor3():
-    motor3 = Motor(dirPin=4, PWMPin=5)  # Motor 3 is controlled from Motor Driv2 #1, which is on GP4/5
+def RotateCW(duty):
+    ina1.value(1)
+    ina2.value(0)
+    inb1.value(1)
+    inb2.value(0)
+    duty_16 = int((duty*65536)/100)
+    pwma.duty_u16(duty_16)
+    pwmb.duty_u16(duty_16)
 
-    while True:
-        print("Forward")
-        motor3.Forward()
-        sleep(1)
-        print("Reverse")
-        motor3.Reverse()
-        sleep(1)
+def RotateCCW(duty):
+    ina1.value(0)
+    ina2.value(1)
+    inb1.value(0)
+    inb2.value(1)
+    duty_16 = int((duty*65536)/100)
+    pwma.duty_u16(duty_16)
+    pwmb.duty_u16(duty_16)
+
+def StopMotor():
+    ina1.value(0)
+    ina2.value(0)
+    inb1.value(0)
+    inb2.value(0)
+    pwma.duty_u16(0)
+    pwmb.duty_u16(0)
 
 
-if __name__ == "__main__":
-    test_motor3()
+while True:
+    duty_cycle=float(input("Enter pwm duty cycle"))
+    print (duty_cycle)
+    RotateCW(duty_cycle)
+    sleep(5)
+    RotateCCW(duty_cycle)
+    sleep(5)
+    StopMotor()
+    sleep(1)
+
